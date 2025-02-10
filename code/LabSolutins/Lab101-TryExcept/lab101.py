90dc308a0c23c3d62a054f0b35c38bcc4ef30a9a
gald10102 <87383406+gald10102@users.noreply.github.com>
2025-02-10 10:36:16 +0200

    added lab101 try/except solution

---
+++ code/LabSolutins/Lab101-TryExcept/lab101.py
@@ -0,0 +1,15 @@
+class ServerError(Exception):
+    pass
+
+server_list = {"nginx": True ,"docker": False ,"aws": False}
+
+user_input = input("give me a server name: ")
+try:
+    server_list[user_input]
+except:
+    print("Server not found")
+if server_list.__contains__(user_input):
+    if server_list[user_input]:
+        print("Server is up")
+    else:
+        print("Server is down")
